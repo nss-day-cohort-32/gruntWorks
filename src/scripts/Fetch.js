5957d662f9f9894773b131f47fa8e8f072d4ce6c
Colin Sandlin <csandlin010@gmail.com>
2019-04-23 09:25:15 -0500

    fixed burak's key for real for real

--- src/scripts/Fetch.js
+++ src/scripts/Fetch.js
@@ -1,13 +1,13 @@
 getResourcesPark();
 {
   fetch(
-    "https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=dqz4K6UvNyJxLC8XNBDcqdr4s",
+    "https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=${burakKey}",
     { Authorization: { Gruntworks: `${burakKey}` } }
   ).then(parkDataBox => parkDataBox.json());
 }
 
 /*User Input fetch{
-    return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=dqz4K6UvNyJxLC8XNBDcqdr4s&${parkSearchUserInput}=Yes`,
+    return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=${burakKey}&${parkSearchUserInput}=Yes`,
     {Authorization: {Gruntworks: "dqz4K6UvNyJxLC8XNBDcqdr4s"}})
     .then(parkDataContainer => parkDataContainer.json());
 */
