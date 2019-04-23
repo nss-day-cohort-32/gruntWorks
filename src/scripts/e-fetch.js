1df1b04477042b336a42f4c27313802bf9a02675
Daniel Clarke <deliotclarke@gmail.com>
2019-04-23 14:39:21 -0500

    fetch file is working and printing using the render search function

--- src/scripts/e-fetch.js
+++ src/scripts/e-fetch.js
@@ -1,17 +1,25 @@
-const searchItem = "tech";
-const eliotKey = "2S2M6RAC5OSTZR7PQSQJ";
+const searchButton = document.querySelector("#meetups-button");
+const searchItem = document.querySelector("#meetups-search-input");
 
-fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchItem}&location.address=nashville&start_date.keyword=today&expand=venue`, {
-  "headers": {
-    "Accept": "application/json",
-    "Authorization": `Bearer ${eliotKey}`
-  }
-})
-  .then(data => data.json())
-  .then(results => {
-    for (let i = 0; i < 5; i++) {
-      let eventName = results.top_match_events[i].name.text;
-      let eventAddress = results.top_match_events[i].venue.address.address_1;
-      // renderSearchResults(eventName, eventAddress);
+searchButton.addEventListener("click", () => {
+  fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchItem.value}&location.address=nashville&start_date.keyword=today&expand=venue`, {
+    "headers": {
+      "Accept": "application/json",
+      "Authorization": `Bearer ${eliotKey}`
     }
   })
+    .then(data => data.json())
+    .then(results => {
+      if (results.top_match_events === undefined) {
+        alert("Sorry! No results, try again!");
+        searchItem.focus();
+      } else {
+        for (let i = 0; i < 5; i++) {
+          let eventName = results.top_match_events[i].name.text;
+          let eventAddress = results.top_match_events[i].venue.address.address_1;
+          renderSearchResults(eventName, eventAddress);
+
+        }
+      }
+    })
+})
