c2ee6c94dd5e4078640d8b959e6f37e27b0f9242
Colin Sandlin <csandlin010@gmail.com>
2019-04-23 09:06:49 -0500

    fixed some headings/id's in the HTML doc

--- src/scripts/Fetch.js
+++ src/scripts/Fetch.js
@@ -1,8 +1,9 @@
-getResourcesPark()
+getResourcesPark();
 {
-    return fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=dqz4K6UvNyJxLC8XNBDcqdr4s",
-    {Authorization: {Gruntworks: "dqz4K6UvNyJxLC8XNBDcqdr4s"}})
-    .then(parkDataBox => parkDataBox.json());
+  fetch(
+    "https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=dqz4K6UvNyJxLC8XNBDcqdr4s",
+    { Authorization: { Gruntworks: "dqz4K6UvNyJxLC8XNBDcqdr4s" } }
+  ).then(parkDataBox => parkDataBox.json());
 }
 
 /*User Input fetch{
