d0de98d883181877b896b66e639beedee6ddb02f
Colin Sandlin <csandlin010@gmail.com>
2019-04-24 12:59:54 -0500

    added functionality to replace items in the itinerary

--- src/scripts/parks.js
+++ src/scripts/parks.js
@@ -3,33 +3,35 @@ console.log("ParkList");
 const parksSearchBtn = document.querySelector("#parks-button");
 let parksInput = document.querySelector("#parks-search-input");
 
-
 parksSearchBtn.addEventListener("click", e => {
-    var takeParksValue = parksInput.value;
-    parksInput.value = "";
-    renderSearchResultsHere.innerHTML = "";
-    console.log(takeParksValue)
+  var takeParksValue = parksInput.value;
+  parksInput.value = "";
+  renderSearchResultsHere.innerHTML = "";
+  console.log(takeParksValue);
   createResultsHeader();
+  const makeCategory = document.createElement("h4");
+  makeCategory.textContent = "Parks";
+  renderSearchResultsHere.appendChild(makeCategory);
   fetch(
     `https://data.nashville.gov/resource/xbru-cfzi.json?$q=${takeParksValue}`,
     {
-        headers: {
-          Accept: "application/json",
-          "X-App-Token": `${burakKey}`
-        }
+      headers: {
+        Accept: "application/json",
+        "X-App-Token": `${burakKey}`
+      }
     }
-    ).then(entries => entries.json())
-     .then(results => {
-        for (var i = 0; i < 5; i++) {
-            if (results[i] === undefined){
-                continue;
-            }
-            let parkName = results[i].park_name;
-            let parkLoc = results[i].mapped_location_address;
-            console.log(parkName);
-            console.log(parkLoc);
-            renderSearchResults(parkName, parkLoc);
-          }
-        });
+  )
+    .then(entries => entries.json())
+    .then(results => {
+      for (var i = 0; i < 5; i++) {
+        if (results[i] === undefined) {
+          continue;
+        }
+        let parkName = results[i].park_name;
+        let parkLoc = results[i].mapped_location_address;
+        console.log(parkName);
+        console.log(parkLoc);
+        renderSearchResults(parkName, parkLoc, "parks");
+      }
     });
-    
+});
